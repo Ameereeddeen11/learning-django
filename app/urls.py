6dc615ed37309d62918eec4fa0de60e8a06a4d78
Amir <amir.abdullaev@email.cz>
2022-03-22 21:50:41 +0100

    start new project on django than setup, installation and page navigation

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path 
+from multiprocessing.spawn import import_main_path
+from nturl2path import url2pathname
+from unicodedata import name
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Create your views here.
 
-def index(request):
-    return HttpResponse("This is the fist view")
+
+def index(response):
+    return HttpResponse("<h1>Amiriddin</h1>")
