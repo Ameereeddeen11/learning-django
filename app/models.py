6c2d3c63ed4b658c227ed65b04b165533093bc92
Amir <amir.abdullaev@email.cz>
2022-03-24 22:32:04 +0100

    django create databases and add some info to the databases

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Brend(models.Model):
+    brend_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name_brend = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name_brend()
