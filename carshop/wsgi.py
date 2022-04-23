ff738f04e00cd045c207e0237042b241af75ab45
Amir <amir.abdullaev@email.cz>
2022-04-23 23:00:03 +0200

    start new project cars and set up django

--- carshop/wsgi.py
+++ carshop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for firtsite project.
+WSGI config for carshop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firtsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carshop.settings')
 
 application = get_wsgi_application()
